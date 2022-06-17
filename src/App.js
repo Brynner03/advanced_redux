df08cf8173472ff6e5b7502e726fe5aa8d59d707
Brynner Ventura <bventura03@hotmail.com>
2022-06-17 00:01:33 -0400

    Initializing Notifications

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
 import { useEffect } from 'react';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
+import { uiActions } from './store/ui-slice'
 
 function App() {
 
+  const dispatch = useDispatch()
   const showCart = useSelector((state) => state.ui.cartIsVisible )
   const cart = useSelector((state) => state.cart)
 
   useEffect(() => {
     const sendCartData = async () => {
-
+      dispatch(uiActions.showNotification({
+        status: 'pending',
+        title: 'Sending',
+        message: 'Sending cart data!'
+      }))
       const response = await fetch(
         'https://redux-practice-8e7e6-default-rtdb.firebaseio.com/cart.json',
         { 
@@ -25,9 +31,20 @@ function App() {
         throw new Error('Sending cart failed..')
       }
 
-      const responseData = await response.json()
+      dispatch(uiActions.showNotification({
+        status: 'success',
+        title: 'Success',
+        message: 'Sent cart successfully!'
+      }))
     }
-  },[cart])
+    sendCartData().catch((error) => {
+      dispatch(uiActions.showNotification({
+        status: 'error',
+        title: 'Error!',
+        message: 'Sending cart data has failed!!'
+      }))
+    })
+  },[cart, dispatch])
 
   return (
     <Layout>
