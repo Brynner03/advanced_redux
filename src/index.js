e8af1c2ec57bc847c357a6287aa2eb02f649c66f
Brynner Ventura <bventura03@hotmail.com>
2022-06-16 22:30:35 -0400

    Fixed import locations

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import { Provider } from 'react-redux'
 
 import './index.css';
 import App from './App';
-import store from './components/store/index';
+import store from '../src/store/index';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<Provider store={store}><App /></Provider>);
